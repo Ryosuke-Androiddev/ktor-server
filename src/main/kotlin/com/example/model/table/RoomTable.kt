3295ddf2d282829127deaa291b1a2545066780f5
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2021-10-10 23:09:40 +0900

    db setup

--- src/main/kotlin/com/example/model/table/RoomTable.kt
+++ src/main/kotlin/com/example/model/table/RoomTable.kt
@@ -1,2 +1,16 @@
 package com.example.model.table
 
+import com.example.model.table.VocabularyTable.primaryKey
+import org.ktorm.schema.Table
+import org.ktorm.schema.boolean
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
+
+object RoomTable: Table<Nothing>("room") {
+
+    val roomId = int("roomId").primaryKey()
+    val roomName = varchar("roomName")
+    val description = varchar("description")
+    var isStudying = boolean("isStudying")
+    var isFinished = boolean("isFinished")
+}
--- src/main/kotlin/com/example/route/VocabularyRoute.kt
+++ src/main/kotlin/com/example/route/VocabularyRoute.kt
@@ -1,23 +1,46 @@
 package com.example.route
 
+import com.example.model.Room
 import com.example.model.Vocabulary
+import com.example.model.table.RoomTable
 import com.example.model.table.VocabularyTable
 import com.example.repository.DatabaseConnection
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import org.ktorm.dsl.from
-import org.ktorm.dsl.map
-import org.ktorm.dsl.select
+import org.ktorm.dsl.*
 
 fun Route.getWords(){
 
     val db = DatabaseConnection.database
 
-    route("/"){
+    route("/words"){
+
         get {
-            val words = db.from(VocabularyTable).select()
+            val room = db.from(RoomTable).select()
+                .map {
+                    val roomId = it[RoomTable.roomId]
+                    val roomName = it[RoomTable.roomName]
+                    val description = it[RoomTable.description]
+                    val isStudying = it[RoomTable.isStudying]
+                    val isFinished = it[RoomTable.isFinished]
+                    Room(
+                        roomId = roomId ?: 0,
+                        roomName = roomName ?: "",
+                        description = description ?: "",
+                        isStudying = isStudying ?: false,
+                        isFinished = isFinished ?: false
+                    )
+                }
+            call.respond(HttpStatusCode.OK,room)
+        }
+
+        get("/{roomId}") {
+            val roomId = call.parameters["roomId"]?.toInt() ?: -1
+            val words = db.from(VocabularyTable)
+                .select()
+                .where { VocabularyTable.roomId eq  roomId }
                 .map {
                     val word = it[VocabularyTable.word]
                     val meaning = it[VocabularyTable.meaning]
